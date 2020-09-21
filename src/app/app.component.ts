35cb367e055dc66bfd00782068271a5abf6f69f4
MrKmall <kemal52987@gmail.com>
2020-09-21 16:34:55 +0700

    on progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpDataService } from 'src/app/services/http-data.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private httpDataService: HttpDataService) { }
+
+  dataSource = new MatTableDataSource();
   title = 'AngularProject';
+  tableColumns  :  string[] = ['id', 'nik', 'name', 'divisionsId', 'positionsId', 'type', 'lastPosition', 'createDate', 'action'];
+
+  ngOnInit(): void {
+    // Fetch All Students on Page load
+    this.getAllStudents()
+  }
+
+  getAllStudents() {
+    this.httpDataService.getList().subscribe((response: any) => {
+      this.dataSource.data = response;
+      console.log(response);
+    });
+  }
 }
